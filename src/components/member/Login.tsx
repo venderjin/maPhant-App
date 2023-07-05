0c497417bed94005e457a39d76d4a5b687a35b28
ojingjing <wlddj14@naver.com>
2023-07-05 13:24:22 +0900

    Routes 생성

---
+++ src/components/member/Login.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Login = () => {
+  return (
+    <View>
+      <Text>hello login page</Text>
+    </View>
+  );
+};
+
+export default Login;
