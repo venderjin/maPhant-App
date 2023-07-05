9904ef5738ac43d8a4463b4ad209de18576e5128
dotz0ver <opir0oui@gmail.com>
2023-07-05 16:59:19 +0900

    Feat: Create signup function

--- src/component/member/SignupFunc.tsx
+++ src/component/member/SignupFunc.tsx
@@ -1,122 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { getAuth } from "firebase/auth";
+export const authService = getAuth();
 import {
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from "react-native";
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
 
-export default function App() {
+const Signup = () => {
+  const [newAccount, setNewAccount] = useState();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [passwordConfirm, setPasswordConfirm] = useState("");
-  const [nickname, setNickname] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [studentNumber, setStudentNumber] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      // 유저 상태 변화 있을 때 실행되는 메소드
+      if (user) {
+        // 로그인 상태
+        setIsLoggedIn(true);
+      } else {
+        // 로그아웃 상태
+        setIsLoggedIn(false);
+      }
+    });
+  }, []);
 
-  // value : textinput 현재 값 - setter를 통해 변경
-  // setter : textinput 값 변경 시 호출되는 콜백 함수 ; 새로운 값으로 업데이트를 위함
-  // placeholder : 임시 텍스트
-  const handleInputChange = (value: string, setter: any) => {
-    setter(value);
+  const Signup = () => {
+    if (newAccount) {
+      createUserWithEmailAndPassword(authService, email, password)
+        .then(() => {
+          // Signin
+          if (authService.currentUser != null)
+            updateProfile(authService.currentUser, {
+              displayName: displayName,
+            });
+        })
+        .catch((error) => alert(error.message));
+      setEmail("");
+      setPassword("");
+      setDisplayName("");
+    } else {
+      signInWithEmailAndPassword(authService, email, password).catch((error) =>
+        alert(error.message)
+      );
+    }
+    setEmail("");
+    setPassword("");
   };
 
-  const renderTextInputWithLabel = (
-    placeholder: any,
-    value: string,
-    setter: any
-  ) => {
-    const [isFocused, setIsFocused] = useState(false);
+  return <></>;
+};
 
-    const handleFocus = () => {
-      setIsFocused(true);
-    };
-
-    const handleBlur = () => {
-      setIsFocused(false);
-    };
-
-    return (
-      <View>
-        <Text
-          style={[
-            styles.label,
-            {
-              top: isFocused || value ? 5 : 20,
-              fontSize: isFocused || value ? 12 : 16,
-              color: isFocused ? "#000" : "#aaa",
-            },
-          ]}
-        >
-          {placeholder}
-        </Text>
-        <TextInput
-          style={{ ...styles.input, marginTop: isFocused ? 25 : 20 }}
-          placeholder={placeholder}
-          value={value}
-          onChangeText={(text) => handleInputChange(text, setter)}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-        />
-      </View>
-    );
-  };
-
-  return (
-    <View style={styles.container}>
-      {renderTextInputWithLabel("email", email, setEmail)}
-      {renderTextInputWithLabel("pw", password, setPassword)}
-      {renderTextInputWithLabel(
-        "pw confirm",
-        passwordConfirm,
-        setPasswordConfirm
-      )}
-      {renderTextInputWithLabel("nickname", nickname, setNickname)}
-      {renderTextInputWithLabel("phone number", phoneNumber, setPhoneNumber)}
-      {renderTextInputWithLabel(
-        "student number",
-        studentNumber,
-        setStudentNumber
-      )}
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.signup}> Signup</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    paddingHorizontal: 40,
-  },
-  input: {
-    backgroundColor: "#f2f2f2",
-    paddingVertical: 15,
-    paddingHorizontal: 20,
-    borderRadius: 30,
-    marginTop: 20,
-    fontSize: 18,
-  },
-  label: {
-    position: "absolute",
-    left: 10,
-    fontSize: 16,
-    color: "#aaa",
-    backgroundColor: "transparent",
-  },
-  signup: {
-    color: "white",
-    textAlign: "center",
-  },
-  button: {
-    backgroundColor: "#000",
-    paddingVertical: 15,
-    paddingHorizontal: 20,
-    borderRadius: 30,
-    marginTop: 40,
-  },
-});
+export default Signup;
