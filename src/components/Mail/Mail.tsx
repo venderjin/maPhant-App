cfa5177d2f869673e9614a263ec73c7892008fb8
jmlee119 <jmlee119@naver.com>
2023-07-05 16:05:57 +0900

    git ignore 추가

--- src/components/Mail/Mail.tsx
+++ src/components/Mail/Mail.tsx
@@ -1,12 +1,41 @@
-import React from "react";
-import { View, Text } from "react-native";
+// import React from "react";
+// import { View, Text, StyleSheet, ScrollView } from "react-native";
+// import { StatusBar } from "expo-status-bar";
+// function Mail() {
+//   return (
+//     <View style={styles.container}>
+//       <StatusBar style="auto" />
+//       <View style={styles.header}>
+//         <Text style={styles.mailText}>쪽지함</Text>
+//       </View>
+//       <ScrollView style={styles.scroll}>
+//         <View style={styles.content}></View>
+//       </ScrollView>
+//     </View>
+//   );
+// }
 
-function Mail() {
-  return (
-    <View>
-      <Text>Mail Test</Text>
-    </View>
-  );
-}
-
-export default Mail;
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     // margin: "5%",
+//     // backgroundColor: "yellow",
+//   },
+//   header: {
+//     flexDirection: "row",
+//     marginTop: "15%",
+//     marginLeft: "8%",
+//   },
+//   mailText: {
+//     fontSize: 30,
+//     fontWeight: "600",
+//   },
+//   scroll: {
+//     backgroundColor: "black",
+//   },
+//   content: {
+//     backgroundColor: "white",
+//     // marginLeft: "5%",
+//   },
+// });
+// export default Mail;
