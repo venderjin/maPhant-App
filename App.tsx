fa79f5296a5c2e6190369267ddf5b5030da84b0c
Choi HyeonSu <sunavy900@gmail.com>
2023-07-12 14:10:28 +0900

    List.tsx에 plus btn 추가 & board.ts에 List 추가

--- App.tsx
+++ App.tsx
@@ -3,19 +3,18 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import BoardList from "./src/App/Board/index";
-import List from "./src/App/Board/List";
 import TotalBoard from "./src/App/Board/TotalList";
 import QnABoard from "./src/App/Board/QnAList";
-
+import DetailBoardList from "./src/App/Board/List";
 const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Board" component={BoardList} />
-        <Stack.Screen name="List" component={List} />
         <Stack.Screen name="TotalBoard" component={TotalBoard} />
         <Stack.Screen name="QnABoard" component={QnABoard} />
+        <Stack.Screen name="List" component={DetailBoardList} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/App/Board/List.tsx
+++ src/App/Board/List.tsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, View, ScrollView, Pressable } from "react-native";
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  Pressable,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 
 import PostSummary from "../../components/Board/PostSummary";
 import { BoardPost, BoardPostMockup } from "../../types/Board";
 import { listArticle } from "../../Api/board";
 
+import { Entypo } from "@expo/vector-icons";
 const DetailList = () => {
   const [boardData, setboardData] = useState<BoardPostMockup[]>([]);
   useEffect(() => {
@@ -14,15 +22,24 @@ const DetailList = () => {
   }, []);
 
   return (
-    <ScrollView style={styles.container}>
-      {boardData.map((board) => (
-        <View key={board.id} style={styles.body}>
-          <Pressable onPress={() => console.log(board.title)}>
-            <PostSummary post={board} />
-          </Pressable>
-        </View>
-      ))}
-    </ScrollView>
+    <View style={styles.container}>
+      <ScrollView>
+        {boardData.map((board) => (
+          <View key={board.id} style={styles.body}>
+            <Pressable onPress={() => console.log(board.title)}>
+              <PostSummary post={board} />
+            </Pressable>
+          </View>
+        ))}
+      </ScrollView>
+      <View style={styles.btn}>
+        <TouchableOpacity>
+          <Text>
+            <Entypo name="plus" size={24} color="black" />
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
@@ -37,6 +54,15 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: "#f2f2f2",
     paddingVertical: 10,
+  },
+  btn: {
+    backgroundColor: "#e9ecef",
+    borderRadius: 30,
+    zIndex: 99,
+    position: "absolute",
+    right: "10%",
+    bottom: "5%",
+    padding: 10,
   },
 });
 export default DetailList;
