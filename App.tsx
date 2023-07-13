125004adad8bc2e5f2e1c7f5d2c7234a9617669f
ur <dorosi150@gmail.com>
2023-07-13 14:50:37 +0900

    Fix: Change import
    member -> Member

--- App.tsx
+++ App.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Home from "./src/components/Home";
-import BoardList from "./src/App/Board/index";
 import Mail from "./src/components/Mail/Mail";
-import Mypage from "./src/components/member/Mypage";
-import { AntDesign, FontAwesome5, Entypo, Ionicons } from "@expo/vector-icons";
-import { NavigationProp } from "@react-navigation/native";
+import Mypage from "./src/components/Member/Mypage";
+import { AntDesign, FontAwesome5, Ionicons } from "@expo/vector-icons";
 import { createStackNavigator } from "@react-navigation/stack";
-import Alarm from "./src/components/Alarm";
 import Routes from "./src/components/navigator/Routes";
-import SignupRoutes from "./src/components/navigator/SignupRoutes";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const HomeStack = () => (
   <Stack.Navigator>
-    <Stack.Screen name="Home" component={Routes[0].component} options={{ headerShown: false }} />
+    <Stack.Screen
+      name="Home"
+      component={Routes[0].component}
+      options={{ headerShown: false }}
+    />
     <Stack.Screen name="alarm" component={Routes[2].component} />
   </Stack.Navigator>
 );
@@ -34,18 +33,46 @@ const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator initialRouteName="Home">
-        <Tab.Screen name="홈" component={HomeStack} options={{ headerShown: false, tabBarIcon: () => <AntDesign name="home" size={30} color="#5299EB" /> }} />
+        <Tab.Screen
+          name="홈"
+          component={HomeStack}
+          options={{
+            headerShown: false,
+            tabBarIcon: () => (
+              <AntDesign name="home" size={30} color="#5299EB" />
+            ),
+          }}
+        />
         <Tab.Screen
           name="게시판"
           component={BoardListStack}
-          options={{ headerShown: false, tabBarIcon: () => <FontAwesome5 name="list-ul" size={30} color="#5299EB" /> }}
+          options={{
+            headerShown: false,
+            tabBarIcon: () => (
+              <FontAwesome5 name="list-ul" size={30} color="#5299EB" />
+            ),
+          }}
         />
         {/* <Tab.Screen name="쪽지" component={Mail} options={{ headerShown: false, tabBarIcon: () => <Entypo name="chat" size={30} color="black" /> }} /> */}
-        <Tab.Screen name="Mail" component={Mail} options={{ headerShown: false, tabBarIcon: () => <AntDesign name="mail" size={24} color="#5299EB" /> }} />
+        <Tab.Screen
+          name="Mail"
+          component={Mail}
+          options={{
+            headerShown: false,
+            tabBarIcon: () => (
+              <AntDesign name="mail" size={24} color="#5299EB" />
+            ),
+          }}
+        />
         <Tab.Screen
           name="내 정보"
           component={Mypage}
-          options={{ headerShown: false, tabBarIcon: () => <Ionicons name="person-outline" size={30} color="#5299EB" /> }}
+          options={{
+            headerShown: false,
+            tabBarIcon: () => (
+              <Ionicons name="person-outline" size={30} color="#5299EB" />
+            ),
+          }}
         />
       </Tab.Navigator>
     </NavigationContainer>
--- src/components/navigator/Routes.tsx
+++ src/components/navigator/Routes.tsx
@@ -1,10 +1,8 @@
-import Login from "../member/Login";
+import Login from "../Member/Login";
 import Home from "../Home";
-import QA from "../Board/QAdetail";
 import Alarm from "../Alarm";
 import QnABoard from "../../App/Board/QnAList";
 import BoardList from "../../App/Board";
-import { Component } from "react";
 import DetailList from "../../App/Board/List";
 import QAdetail from "../Board/QAdetail";
 // ... import all other pages
