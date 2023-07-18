b4d3832b26150e5a679123e6d131c024d692fa69
ur <dorosi150@gmail.com>
2023-07-18 16:23:48 +0900

    Feat: Create Routes
    App/Signup/Index.tsx, App/Login/Index.tsx

---
+++ src/App/Login/Index.tsx
@@ -0,0 +1,11 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import SigninRoutes from "../../Navigator/SigninRoutes";
+
+const Stack = createStackNavigator();
+
+export default () => (
+  <Stack.Navigator initialRouteName="Signp">
+    <Stack.Screen name="Signin" component={SigninRoutes[0].component} />
+    <Stack.Screen name="Find" component={SigninRoutes[1].component} />
+  </Stack.Navigator>
+);
