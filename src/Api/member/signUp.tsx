9e277f98adfe142677f7e8bc0287ce08984d774f
ur <dorosi150@gmail.com>
2023-07-19 15:42:06 +0900

    Feat: 조건에 맞는 에러 메시지 추가

--- src/Api/member/signUp.tsx
+++ src/Api/member/signUp.tsx
@@ -3,8 +3,21 @@ import constraints from "../constraints";
 function validateEmail(email: String) {
   return fetch(`${constraints.SERVER_URL}/user/validation/email`, {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify({ email: email }),
   }).then(response => response.json());
 }
 
-export { validateEmail };
+function validateNickname(nickname: String) {
+  return fetch(`${constraints.SERVER_URL}/user/validation/nickname`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nickname: nickname }),
+  }).then(response => response.json());
+}
+
+export { validateEmail, validateNickname };
--- src/App/Signup/Signup.tsx
+++ src/App/Signup/Signup.tsx
@@ -5,6 +5,7 @@ import { Formik, Field } from "formik";
 import * as Yup from "yup";
 import CustomInput from "../../components/Member/CustomInput";
 import Search from "../../components/Member/Search";
+import { validateEmail, validateNickname } from "../../Api/member/signUp";
 
 interface ISignupForm {
   email: string;
@@ -20,17 +21,33 @@ const validationSchema = Yup.object().shape({
   email: Yup.string()
     .matches(
       /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/,
-      "이메일 형식으로 입력해주세요",
+      "학교 이메일 형식으로 입력해주세요(.ac.kr 또는 .edu)",
     )
-    .required("이메일은 필수 항목입니다."),
+    .required("이메일은 필수 항목입니다.")
+    .test(async (value, testContext) => {
+      let result = await validateEmail(value);
+      if (result.success) return true;
+      return testContext.createError({ message: result.errors });
+    }),
   password: Yup.string()
+    .matches(/\w*[a-z]\w*/, "비밀번호에는 소문자가 포함되어야 합니다.")
+    .matches(/\w*[A-Z]\w*/, "비밀번호에는 대문자가 포함되어야 합니다.")
+    .matches(/\d/, "비밀번호에는 숫자가 포함되어야 합니다.")
+    .matches(/[!@#$%^&*()\-_"=+{}; :,<.>]/, "비밀번호에는 특수문자가 포함되어야 합니다.")
     .required("필수 정보입니다.")
-    .min(6, ({ min }) => `비밀번호는 최소 ${min}자 이상이어야 합니다.`),
+    .min(8, ({ min }) => `비밀번호는 최소 ${min}자 이상이어야 합니다.`),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref("password")], "비밀번호가 일치하지 않습니다.")
     .required("필수 정보입니다."),
   phoneNumber: Yup.string().matches(/^\d{3}-\d{4}-\d{4}$/, "000-0000-0000 형식으로 입력해주세요."),
-  nickname: Yup.string().required("필수 정보입니다."),
+  nickname: Yup.string()
+    // .matches(/^[a-zA-Z0-9가-힣_-]{3,20}$/, "닉네임은 3자 이상 20자 이하이어야 합니다.")
+    .required("필수 정보입니다.")
+    .test(async (value, testContext) => {
+      let result = await validateNickname(value);
+      if (result.success) return true;
+      return testContext.createError({ message: result.errors });
+    }),
   studentNumber: Yup.string().required("필수 정보입니다."),
   university: Yup.string().required("필수 정보입니다."),
 });
@@ -64,7 +81,7 @@ const Signup = () => {
               secureTextEntry
             />
             <Field placeholder="닉네임" name="nickname" component={CustomInput} />
-            {/* <Field placeholder="전화번호" name="phoneNumber" component={CustomInput} /> */}
+            <Field placeholder="전화번호" name="phoneNumber" component={CustomInput} />
             <Field placeholder="Search Univ..." name="university" component={Search} />
             <Field placeholder="학번" name="studentNumber" component={CustomInput} />
             <TouchableOpacity
