ac70f82c29b8b166118eb1e9a80dd04c7a018528
KorMJ <alswh456123@kyungsung.ac.kr>
2023-08-01 15:25:17 +0900

    Feat : splash screen setup

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import { ActivityIndicator } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Mail from "./src/App/Mail/Mail";
@@ -9,11 +9,8 @@ import Home from "./src/App/Home/Index";
 import BoardListStack from "./src/App/Board/index";
 import Login from "./src/App/Login/Index";
 import { Provider, useSelector, useDispatch } from "react-redux";
-import AppLoading from "expo-app-loading";
 import reduxStore, { userSlice } from "./src/storage/reduxStore";
 import UserStorage from "./src/storage/UserStorage";
-import UIStore from "./src/storage/UIStore";
-import Spinner from "react-native-loading-spinner-overlay";
 
 const Tab = createBottomTabNavigator();
 
@@ -40,12 +37,38 @@ const App = () => {
           if (res) {
             UserStorage.setUserProfile(res);
           }
+          setIsLoadingComplete(true);
         });
+      } else {
+        setIsLoadingComplete(true);
       }
     });
   }, []);
 
+  // useEffect(() => {
+  //   UserStorage.getUserToken().then(res => {
+  //     let token = null;
+  //     let privKey = null;
+
+  //     token = res ? res.token : null;
+  //     privKey = res ? res.privKey : null;
+
+  //     if (token != null && privKey != null) {
+  //       UserStorage.setUserToken(token, privKey);
+  //       UserStorage.getUserProfile().then(res => {
+  //         if (res) {
+  //           UserStorage.setUserProfile(res);
+  //         }
+  //       });
+  //     }
+  //   });
+  // }, []);
+
   const isLogged = useSelector(UserStorage.isUserLoggedInSelector);
+
+  if (!isLoadingComplete) {
+    return <SplashScreen />;
+  }
 
   if (isLogged == false) {
     return (
@@ -56,7 +79,6 @@ const App = () => {
   }
 
   return (
-    // Stx
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen
