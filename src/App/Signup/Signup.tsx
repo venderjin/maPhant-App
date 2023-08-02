a5386cb46ed6a390d7341af5a5ef8653e79da823
jmlee119 <jmlee119@naver.com>
2023-08-02 14:15:37 +0900

    Fix : Signup.tsx type

--- src/App/Signup/Signup.tsx
+++ src/App/Signup/Signup.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 // import { SearchBar } from "@rneui/themed";
-import { StyleSheet, Text, TouchableOpacity, KeyboardAvoidingView, ScrollView } from "react-native";
+import { ScrollView } from "react-native";
 import { Formik, Field } from "formik";
 import * as Yup from "yup";
 import CustomInput from "../../components/Member/CustomInput";
@@ -13,7 +13,6 @@ import {
   validatePassword,
 } from "../../Api/member/signUp";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 interface ISignupForm {
   email: string;
@@ -26,7 +25,6 @@ interface ISignupForm {
   university: string;
 }
 import { Spacer, Container, TextButton } from "../../components/common";
-import DropDownPicker from "react-native-dropdown-picker";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string()
@@ -36,7 +34,7 @@ const validationSchema = Yup.object().shape({
     )
     .required("학교 이메일 형식으로 입력해주세요(.ac.kr 또는 .edu)")
     .test(async (value, testContext) => {
-      let result = await validateEmail(value);
+      const result = await validateEmail(value);
       if (result.success) return true;
       return testContext.createError({ message: result.errors });
     }),
@@ -49,7 +47,7 @@ const validationSchema = Yup.object().shape({
     .min(8, ({ min }) => `비밀번호는 최소 ${min}자 이상이어야 합니다.`)
     .required("영문 대, 소문자, 숫자, 특수문자 1개 이상 으로 구성된 8자 이상으로 입력해주세요.")
     .test(async (value, testContext) => {
-      let result = await validatePassword(value);
+      const result = await validatePassword(value);
       if (result.success) return true;
       return testContext.createError({ message: result.errors });
     }),
@@ -61,7 +59,7 @@ const validationSchema = Yup.object().shape({
     // .matches(/^[a-zA-Z0-9가-힣_-]{3,20}$/, "닉네임은 3자 이상 20자 이하이어야 합니다.")
     .required("필수 정보입니다.")
     .test(async (value, testContext) => {
-      let result = await validateNickname(value);
+      const result = await validateNickname(value);
       if (result.success) return true;
       return testContext.createError({ message: result.errors });
     }),
@@ -113,7 +111,7 @@ const Signup = () => {
               });
           }}
         >
-          {({ handleSubmit, isValid, values }) => (
+          {({ handleSubmit }) => (
             <Container
               style={{
                 flex: 1,
