795ceb8492af0deb28d1fb202e4b93f7bb000e07
ojingjing <wlddj14@naver.com>
2023-08-03 17:44:26 +0900

    채팅방 UI완성

--- src/App/Mail/Chatroom.tsx
+++ src/App/Mail/Chatroom.tsx
@@ -4,33 +4,75 @@ import { ScrollView } from "react-native";
 import { Container, ImageBox, Input, Spacer, TextButton } from "../../components/common";
 
 const Chatroom: React.FC = () => {
-  const chatData = { profile: "user", name: "User", time: "10:00 AM", content: "Hello" };
-  const chatComponents = Array(100).fill(chatData);
+  // const chatData = { profile: "user", name: "User", time: "10:00 AM", content: "Hello" };
+  // const chatComponents = Array(100).fill(chatData);
+
+  function UserChat() {
+    return (
+      <Container style={{ paddingVertical: 0 }}>
+        <Container style={{ padding: 10 }}>
+          <Text>Username</Text>
+          <Container style={{ flexDirection: "row", alignItems: "flex-end" }}>
+            <Container
+              style={{
+                backgroundColor: "rgba(82, 153, 235, 0.3)",
+                paddingVertical: 13,
+                paddingHorizontal: 20,
+                borderRadius: 10,
+                flexShrink: 1,
+              }}
+            >
+              <Text>안녕하세요</Text>
+            </Container>
+            <Text style={{ marginLeft: 5 }}>date</Text>
+          </Container>
+        </Container>
+      </Container>
+    );
+  }
+  function OtherUserChat() {
+    return (
+      <Container style={{ paddingVertical: 0 }}>
+        <Container style={{ padding: 10, alignItems: "flex-end" }}>
+          <Text>Username</Text>
+          <Container style={{ flexDirection: "row", alignItems: "flex-end" }}>
+            <Text style={{ marginRight: 5 }}>date</Text>
+            <Container
+              style={{
+                backgroundColor: "#5299EB",
+                paddingVertical: 13,
+                paddingHorizontal: 20,
+                borderRadius: 10,
+                flexShrink: 1,
+              }}
+            >
+              <Text style={{ color: "white" }}>안녕하세dfdfsdfdsdfasdfadfadsfasddfasdfasdf요</Text>
+            </Container>
+          </Container>
+        </Container>
+      </Container>
+    );
+  }
   return (
     <Container style={{ flex: 1, display: "flex" }}>
       <Container // 채팅방 이름
         style={{
           flex: 0.8,
-          backgroundColor: "green",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
         <Text style={{ fontSize: 23, fontWeight: "bold" }}>채팅방이름</Text>
       </Container>
-      <Container style={{ flex: 10, backgroundColor: "pink" }}>
+      <Container style={{ flex: 10 }}>
         <ScrollView>
-          {chatComponents.map((chat, index) => (
-            <Text key={index} style={styles.chatText}>
-              {`${chat.profile} (${chat.name}) - ${chat.time}: ${chat.content}`}
-            </Text>
-          ))}
+          <UserChat />
+          <OtherUserChat />
         </ScrollView>
       </Container>
       <Container // 채팅입력창
         style={{
           flex: 1,
-          backgroundColor: "purple",
           flexDirection: "row",
           padding: "3%",
         }}
