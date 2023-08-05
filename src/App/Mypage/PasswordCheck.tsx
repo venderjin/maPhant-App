9357e53abf0b0d256c11abf6277f32fccf7cb6fa
venderjin <lffpz3331@gmail.com>
2023-08-05 19:27:34 +0900

    회원정보 ui 수정

--- src/App/Mypage/PasswordCheck.tsx
+++ src/App/Mypage/PasswordCheck.tsx
@@ -1,10 +1,10 @@
 import { useNavigation } from "@react-navigation/native";
 import React, { useState } from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 import { Container, Input, Spacer, TextButton } from "../../components/common";
 
-const ProfileModify: React.FC = () => {
+const PasswordCheck: React.FC = () => {
   const [password, setPassword] = useState("");
   const navigation = useNavigation<NavigationProps>();
   //로그인된  회원의 email을 서버로 받고 user가 입력한 비밀번호를 넘겨서 check
@@ -20,14 +20,23 @@ const ProfileModify: React.FC = () => {
         keyboardShouldPersistTaps="handled"
       >
         <View>
-          <Spacer size={100} />
+          <Spacer size={80} />
           <Container style={{ width: "100%" }} isItemCenter={true} paddingHorizontal={0}>
-            <Spacer size={30} />
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Text style={{ fontSize: 18 }}> 회원정보 수정 전 비밀번호를 확인해주세요</Text>
+            </View>
+            <Spacer size={25} />
             <Input
               style={{ paddingVertical: "5%" }}
               paddingHorizontal={20}
               borderRadius={30}
-              placeholder="회원정보 수정 전 비밀번호를 확인해주세요"
+              placeholder="Password"
               onChangeText={text => setPassword(text)}
               value={password}
               secureTextEntry={true}
@@ -54,4 +63,4 @@ const styles = StyleSheet.create({
     marginTop: 18,
   },
 });
-export default ProfileModify;
+export default PasswordCheck;
--- src/App/Mypage/ProfileModify.tsx
+++ src/App/Mypage/ProfileModify.tsx
@@ -1,36 +1,76 @@
-import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, Text, View } from "react-native";
 
-import { Container, TextButton } from "../../components/common";
+import { Container, Input, TextButton } from "../../components/common";
 
 const ProfileModify: React.FC = () => {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
   return (
-    <Container isForceKeyboardAvoiding={true} style={{ backgroundColor: "white" }}>
+    <Container isFullScreen={true} paddingHorizontal={0} style={{ backgroundColor: "white" }}>
       <ScrollView
+        contentContainerStyle={{
+          flexDirection: "column",
+          flexGrow: 1,
+          justifyContent: "space-between",
+          paddingHorizontal: 16,
+        }}
         keyboardShouldPersistTaps="handled"
-        contentContainerStyle={{ backgroundColor: "white" }}
-      ></ScrollView>
-      <TextButton
-        backgroundColor="#5299EB"
-        fontColor="white"
-        paddingHorizontal={20}
-        paddingVertical={15}
-        borderRadius={30}
-        fontSize={18}
-        onPress={() => {}}
       >
-        저장하기
-      </TextButton>
-      <TextButton onPress={() => {}}>확인하기</TextButton>
+        <View>
+          <Text style={{ fontSize: 18, padding: 10 }}>이메일</Text>
+          <Text style={{ fontSize: 18, padding: 10 }}>비밀번호</Text>
+          <Input
+            style={{ paddingVertical: "5%", backgroundColor: "#D8E1EC" }}
+            paddingHorizontal={20}
+            borderRadius={30}
+            placeholder="password"
+            onChangeText={text => setPassword(text)}
+            value={password}
+            secureTextEntry={true}
+          ></Input>
+          <Text style={{ fontSize: 18, padding: 10 }}>비밀번호 확인</Text>
+          <Input
+            style={{ paddingVertical: "5%", backgroundColor: "#D8E1EC" }}
+            paddingHorizontal={20}
+            borderRadius={30}
+            placeholder="confirmPassword"
+            onChangeText={text => setConfirmPassword(text)}
+            value={confirmPassword}
+            secureTextEntry={true}
+          ></Input>
+          <Text style={{ fontSize: 18, padding: 10 }}>닉네임</Text>
+          <Input
+            style={{ paddingVertical: "5%", backgroundColor: "#D8E1EC" }}
+            paddingHorizontal={20}
+            borderRadius={30}
+            placeholder="nickname"
+            onChangeText={text => setNickname(text)}
+            value={nickname}
+            secureTextEntry={true}
+          ></Input>
+          <Text style={{ fontSize: 18, padding: 10 }}>이름</Text>
+          <Text style={{ fontSize: 18, padding: 10 }}>학번</Text>
+          <Text style={{ fontSize: 18, padding: 10 }}>핸드폰 번호</Text>
+          <Input
+            style={{ paddingVertical: "5%", backgroundColor: "#D8E1EC" }}
+            paddingHorizontal={20}
+            borderRadius={30}
+            placeholder=" PhoneNumber"
+            onChangeText={text => setPhoneNumber(text)}
+            value={phoneNumber}
+            secureTextEntry={true}
+          ></Input>
+          <Text style={{ fontSize: 18, padding: 10 }}>계열</Text>
+          <Text style={{ fontSize: 18, padding: 10 }}>학과</Text>
+          <TextButton onPress={() => {}}>저장 </TextButton>
+        </View>
+      </ScrollView>
     </Container>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 16,
-    paddingVertical: 30,
-    marginTop: 18,
-  },
-});
 export default ProfileModify;
