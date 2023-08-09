42fc67c8e24afa5fb6ddb020577c724ec219a9b6
ojingjing <wlddj14@naver.com>
2023-08-09 14:58:31 +0900

    무한루프 해결 새로운 대화 업데이트시 바로 ui 반영되게 하기

--- src/App/Mail/Chatroom.tsx
+++ src/App/Mail/Chatroom.tsx
@@ -1,6 +1,6 @@
 import { StackActions, useNavigation, useRoute } from "@react-navigation/native";
 import { useEffect, useRef, useState } from "react";
-import { Text } from "react-native";
+import { KeyboardAvoidingView, Text, Platform } from "react-native";
 import { ScrollView } from "react-native";
 import { useWindowDimensions } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
@@ -12,54 +12,59 @@ import { NavigationProps } from "../../Navigator/Routes";
 import { ReceiveList } from "../../types/DM";
 const Chatroom: React.FC = () => {
   const navigation = useNavigation<NavigationProps>();
-  const windowWidth = useWindowDimensions().width; // window 가로 길이
+  const scrollViewRef = useRef<ScrollView>(null);
+
+  const windowWidth = useWindowDimensions().width; // window 가로 길이s
   // SearchUser.tsx에서 입력한 유저의 id, nickname을 가져오기 위해 사용한 것
   const route = useRoute();
   const params = route.params as MailFormParams;
-
-  const [content, setContent] = useState("");
-  const scrollViewRef = useRef<ScrollView>(null);
-
-  // 채팅방 스크롤 하단으로 보내는 기능
-  // const scrollToBottom = () => {
-  //   if (scrollViewRef.current) {
-  //     scrollViewRef.current.scrollToEnd({ animated: true });
-  //   }
-  // };
-
-  // 상대방이 보낸 내용 담는 배열
   const [receiveContent, setReceiveContent] = useState<ReceiveList[]>([]);
-
-  const send = () => {
-    sendContent(params.id, content)
+  const [content, setContent] = useState("");
+  const scrollToBottom = () => {
+    //스크롤
+    if (scrollViewRef.current) {
+      scrollViewRef.current.scrollToEnd({ animated: true });
+    }
+  };
+  useEffect(() => {
+    scrollToBottom();
+  }, []);
+  const send = async () => {
+    // 전송 버튼 눌렸을때 실행되는 함수
+    sendContent(params.id, content) //postApi 로 id ,content 보냄
       .then(res => {
-        if (res.success) return;
+        //성공하면 return 시켜라
+        if (res.success) {
+          fetchChatLists(params.roomId);
+          scrollToBottom();
+        }
+        console.log("send성공", res.data);
       })
-      .catch(e => console.info(e));
+      .catch(e => console.error("send에러", e));
     setContent("");
   };
 
-  useEffect(() => {
-    // 대화방 처음 만들 때는 바로 채팅방 id를 확인 할수 없어서 방에 들어와서 찾아주는 거임
-
-    // if (params.roomId === 0) {
-    //   receiveChatrooms().then(res => {
-    //     if (res.success) setChatroomId(res.data);
-    //   });
-    // }
-    // console.info(chatroomId);
-    // chatroomId[0].id 이렇게 값을 못찾는다...
-
-    //상대방이 보낸 대화내용 불러옴
-    chartLists(params.roomId)
+  const fetchChatLists = async (roomId: number) => {
+    //대화내용 받아오는거 같음
+    chartLists(roomId) //그 대화내용의 방 id
       .then(res => {
         if (res.success) {
           setReceiveContent(res.data?.list);
+          console.log("fetchChatLists 받아옴");
         }
       })
-      .catch(e => console.error(e));
-  }, [receiveContent]);
+      .catch(e => console.error("fetchChatLists에러", e));
+  };
+
+  useEffect(() => {
+    if (params.roomId) {
+      fetchChatLists(params.roomId);
+    }
+  }, [params.roomId]);
+  console.log("징징");
+
   function getCurrentTime(targetDate: Date) {
+    //시간
     const hours = targetDate.getHours();
     const minutes = targetDate.getMinutes();
     // 00 : 00 분으로 표시되게 바꿈
@@ -68,6 +73,7 @@ const Chatroom: React.FC = () => {
     const currentTime = `${formattedHours}:${formattedMinutes}`;
     return currentTime;
   }
+
   function OtherUserChat({ message }: { message: ReceiveList }) {
     return (
       <Container style={{ paddingVertical: 0 }}>
@@ -91,12 +97,11 @@ const Chatroom: React.FC = () => {
       </Container>
     );
   }
-
   function UserChat({ message }: { message: ReceiveList }) {
     return (
       <Container style={{ paddingVertical: 0 }}>
         <Container key={message.id} style={{ padding: 10, alignItems: "flex-end" }}>
-          <Text>Me</Text>
+          <Text>ME</Text>
           <Container style={{ flexDirection: "row", alignItems: "flex-end" }}>
             <Text style={{ marginRight: 5 }}>{getCurrentTime(new Date(message.time))}</Text>
             <Container
@@ -116,60 +121,59 @@ const Chatroom: React.FC = () => {
     );
   }
   return (
-    <Container
-      isFullScreen={true}
-      isForceKeyboardAvoiding={true}
-      style={{ flex: 1, display: "flex" }}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 25}
     >
-      <Container // 채팅방 이름
-        style={{
-          flex: 0.7,
-          alignItems: "center",
-          justifyContent: "space-between",
-          flexDirection: "row",
-          flexShrink: 1,
-        }}
-      >
-        <TouchableOpacity onPress={() => navigation.dispatch(StackActions.popToTop())}>
-          <ImageBox source={require("../../../assets/arrow-circle.png")} width={35}></ImageBox>
-        </TouchableOpacity>
-        <Text style={{ fontSize: 23, fontWeight: "bold", marginRight: windowWidth / 2 - 66 }}>
-          채팅방이름
-        </Text>
-      </Container>
-      <Container style={{ flex: 10 }}>
-        <ScrollView ref={scrollViewRef}>
-          {receiveContent.map(
-            message =>
-              message.time &&
-              (message.is_me ? (
+      <Container isFullScreen={true} style={{ flex: 1, display: "flex" }}>
+        <Container // 채팅방 이름
+          style={{
+            flex: 0.7,
+            alignItems: "center",
+            justifyContent: "space-between",
+            flexDirection: "row",
+            flexShrink: 1,
+          }}
+        >
+          <TouchableOpacity onPress={() => navigation.dispatch(StackActions.popToTop())}>
+            <ImageBox source={require("../../../assets/arrow-circle.png")} width={35}></ImageBox>
+          </TouchableOpacity>
+          <Text style={{ fontSize: 23, fontWeight: "bold", marginRight: windowWidth / 2 - 66 }}>
+            채팅방이름
+          </Text>
+        </Container>
+        <Container style={{ flex: 10 }}>
+          <ScrollView ref={scrollViewRef}>
+            {receiveContent.map(message =>
+              message.time && message.is_me ? (
                 <UserChat key={message.id} message={message} />
               ) : (
                 <OtherUserChat key={message.id} message={message} />
-              )),
-          )}
-        </ScrollView>
-      </Container>
-      <Container // 채팅입력창
-        paddingHorizontal={0}
-        style={{
-          flex: 1,
-          flexDirection: "row",
+              ),
+            )}
+          </ScrollView>
+        </Container>
+        <Container // 채팅입력창
+          paddingHorizontal={0}
+          style={{
+            flex: 2, // 네비게이션 바 없어지면 1로 바꾸기
+            flexDirection: "row",
 
-          // paddingHorizontal: 10,
-          padding: "3%",
-        }}
-      >
-        {/* <Container style={{ flex: 6 }}> */}
-        <Input
-          multiline={true}
-          style={{ maxHeight: 100, flexShrink: 1, flex: 6 }}
-          placeholder="message"
-          value={content}
-          onChangeText={setContent}
-        />
-        {/* </Container> */}
-        {/* <Container
+            // paddingHorizontal: 10,
+            padding: "3%",
+          }}
+        >
+          {/* <Container style={{ flex: 6 }}> */}
+          <Input
+            multiline={true}
+            style={{ maxHeight: 100, flexShrink: 1, flex: 6 }}
+            placeholder="message"
+            value={content}
+            onChangeText={setContent}
+          />
+          {/* </Container> */}
+          {/* <Container
           style={{
             backgroundColor: "pink",
             flex: 1,
@@ -177,25 +181,26 @@ const Chatroom: React.FC = () => {
             alignItems: "center",
           }}
         > */}
-        <TextButton
-          onPress={() => {
-            send();
-          }}
-          style={{
-            flex: 1,
-            marginLeft: 10,
-            paddingHorizontal: 0,
-            paddingVertical: 0,
-            borderRadius: 300,
-            width: "100%",
-            height: "100%",
-          }}
-        >
-          전송
-        </TextButton>
-        {/* </Container> */}
+          <TextButton
+            onPress={() => {
+              send();
+            }}
+            style={{
+              flex: 1,
+              marginLeft: 10,
+              paddingHorizontal: 0,
+              paddingVertical: 0,
+              borderRadius: 300,
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            전송
+          </TextButton>
+          {/* </Container> */}
+        </Container>
       </Container>
-    </Container>
+    </KeyboardAvoidingView>
   );
 };
 
