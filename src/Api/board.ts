a8599f02e79145422ed148c8485ad2415ee2c011
jmlee119 <jmlee119@naver.com>
2023-08-14 16:40:40 +0900

    Fix : Sort

--- src/Api/board.ts
+++ src/Api/board.ts
@@ -63,7 +63,7 @@ const boardDelete = (board_id: number): Promise<dataResponse> =>
   DeleteAPI<dataResponse>(`/board/${board_id}/`);
 
 const getArticle = (board_id: number): Promise<dataResponse<BoardPost>> =>
-  GetAPI<dataResponse<BoardPost>>(`/board/${board_id}`);
+  GetAPI<dataResponse<BoardPost>>(`/board/${board_id}/`);
 
 function insertLikePost(board_id: number) {
   return PostAPI(`/board/like/${board_id}/`);
@@ -82,6 +82,9 @@ function bookMarkArticle(board_id: number) {
 function DeletebookMarkArticle(board_id: number) {
   return DeleteAPI(`/bookmark/${board_id}`);
 }
+function ImageUpload(formData: FormData) {
+  return PostAPI(`/image`, formData);
+}
 const listReportType = (): Promise<dataResponse> => GetAPI<dataResponse>(`/report/list`);
 function ReportPost(board_id: number, reportType_id: number) {
   return PostAPI(`/board/report/?boardId=${board_id}&reportId=${reportType_id}`, {
@@ -89,7 +92,8 @@ function ReportPost(board_id: number, reportType_id: number) {
     reportType_id,
   });
 }
-const listSortCriterion = (): Promise<dataResponse> => GetAPI<dataResponse>(`/board/sortCriterion`);
+const listSortCriterion = (): Promise<dataResponse> =>
+  GetAPI<dataResponse>(`/board/sortCriterion/`);
 
 const commentArticle = (
   board_id: number,
@@ -150,6 +154,7 @@ export {
   DeletebookMarkArticle,
   deleteLikeBoard,
   getArticle,
+  ImageUpload,
   insertLikePost,
   listArticle,
   listBoardType,
