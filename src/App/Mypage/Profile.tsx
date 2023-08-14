ec736d8fde7208846be25de9f6182dfb8b73daae
ojingjing <wlddj14@naver.com>
2023-08-14 20:29:58 +0900

    다른 사람 프로필 ui 완성

--- src/App/Mypage/Profile.tsx
+++ src/App/Mypage/Profile.tsx
@@ -12,47 +12,66 @@ const Profile: React.FC = () => {
 
   function OtherProfile() {
     return (
-      <Container style={styles.Container}>
-        <Container style={{ alignItems: "center" }}>
+      <Container
+        style={{
+          flex: 2,
+          backgroundColor: "rgba(82, 153, 235, 0.3)",
+          borderRadius: 8,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Container>
           <ImageBox
             source={require("../../../assets/image3.png")}
-            width={100}
-            height={100}
+            width={120}
+            height={120}
             borderRadius={100}
+            style={{ borderColor: "#5299EB", borderWidth: 3, borderRadius: 100 }}
           />
         </Container>
-        <Container style={{ alignItems: "center", marginVertical: 5 }}>
+        <Container style={{ alignItems: "center", margin: 20 }}>
           <Container>
             <Text style={{ fontSize: 25, fontWeight: "bold" }}>UserNickName</Text>
           </Container>
           <Container>
             <Text style={{ color: "gray" }}>User department</Text>
           </Container>
-          <Container>
+          <Container style={{ margin: 10 }}>
             <Text>안녕하세요 여기는 소개글 자리입니다.!</Text>
           </Container>
+        </Container>
+        <Container>
+          <TextButton onPress={() => alert("메롱")}>1:1채팅</TextButton>
         </Container>
       </Container>
     );
   }
-  function SendMail() {
+  //   function SendMail() {
+  //     return (
+  //       <Container
+  //         style={{
+  //           backgroundColor: "rgba(82, 153, 235, 0.8)",
+  //           alignItems: "center",
+  //           borderRadius: 8,
+  //           marginTop: 20,
+  //         }}
+  //       >
+  //         <Text style={{ color: "white" }}>쪽지 보내기 </Text>
+  //         <FontAwesome name="message-line" size={24} color="black" />
+  //       </Container>
+  //     );
+  //   }
+  function Otherfunction() {
     return (
       <Container
         style={{
-          backgroundColor: "rgba(82, 153, 235, 0.8)",
-          alignItems: "center",
+          backgroundColor: "rgba(82, 153, 235, 0.3)",
           borderRadius: 8,
+          flex: 1,
           marginTop: 20,
         }}
       >
-        <Text style={{ color: "white" }}>쪽지 보내기 </Text>
-        <FontAwesome name="message-square" size={24} color="black" />
-      </Container>
-    );
-  }
-  function Otherfunction() {
-    return (
-      <Container style={styles.Container}>
         <Container style={{ padding: 20 }}>
           <Container style={{ flexDirection: "row" }}>
             <FontAwesome name="user" color="#5299EB" size={18} />
@@ -91,19 +110,11 @@ const Profile: React.FC = () => {
     );
   }
   return (
-    <SafeAreaView style={{ display: "flex", padding: 20 }}>
+    <SafeAreaView style={{ display: "flex", flex: 1, padding: 20 }}>
       <OtherProfile />
-      <SendMail />
       <Otherfunction />
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  Container: {
-    backgroundColor: "rgba(82, 153, 235, 0.3)",
-    borderRadius: 8,
-    marginTop: 20,
-  },
-});
 export default Profile;
